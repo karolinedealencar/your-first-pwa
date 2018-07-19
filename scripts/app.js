0d3e8e5bfdd1d3fa89e6d2fbe5094d51406ee4af
Karoline Alencar <karoline.alencar@dicasmei.com.br>
2018-07-19 10:16:17 -0300

    Check if the browser supports sservice workers

--- scripts/app.js
+++ scripts/app.js
@@ -326,4 +326,10 @@
   }
 
   // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+  
 })();
